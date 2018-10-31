5ac8abb7d7efb495d5e51d7b0050044cbfe09dd9
Pau <pau@fxstreet.com>
2018-10-31 18:18:16 +0100

    Initial commit

---
+++ XamarinTechFriday/XamarinTechFriday/Views/Steps/FirstStep.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinTechFriday.Views.Steps
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FirstStep : ContentPage
+	{
+		public FirstStep()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ XamarinTechFriday/XamarinTechFriday/Views/Steps/SecondStep.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinTechFriday.Views.Steps
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SecondStep : ContentPage
+	{
+		public SecondStep ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
